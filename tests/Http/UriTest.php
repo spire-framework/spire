eb7a85ec659fde4bdce2a7083e9f42cb53a78649
Chris Pynegar <chris@chrispynegar.co.uk>
2016-05-30 23:50:47 +0100

    Progress on URI class

---
+++ tests/Http/UriTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Spire\Http\Uri;
+
+class UriTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        Uri::initialize();
+    }
+
+    public function testInitialize()
+    {
+
+    }
+
+    public function testBase()
+    {
+
+    }
+
+    public function testUri()
+    {
+
+    }
+
+    public function testSegments()
+    {
+
+    }
+
+    public function testSegment()
+    {
+
+    }
+
+    public function testSegmentString()
+    {
+
+    }
+
+}
