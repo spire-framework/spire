d752784f448616e1758653f61e32ead1e65c40ee
Chris Pynegar <chris@chrispynegar.co.uk>
2016-07-12 17:38:36 +0100

    Add hash class

---
+++ src/Spire/Encryption/Hash.php
@@ -0,0 +1,38 @@
+<?php
+namespace Spire\Encryption;
+
+use Spire\Exception\Exception;
+
+class Hash
+{
+
+    /**
+     * Checks that the algorithm is valid.
+     *
+     * @param  string  $algo  The algorithm to check.
+     * @return bool
+     */
+    public static function validAlgo(string $algo): bool
+    {
+        return in_array($algo, hash_algos());
+    }
+
+    /**
+     * Generates a hash.
+     *
+     * @param  string  $data  Data to hash.
+     * @param  string  $algo  Algorithm method to encrypt with.
+     * @return string
+     */
+    public static function make(string $data, string $algo = 'sha256'): string
+    {
+        // Check that we are using a valid algorithm.
+        if (!self::validAlgo($algo))
+        {
+            throw new Exception(sprintf('Invalid hashing algorithm: %s.', $algo));
+        }
+
+        return hash($algo, $data);
+    }
+
+}
