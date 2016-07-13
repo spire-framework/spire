cdd852b86a27715141cad8feaa8465f65419f675
Chris Pynegar <chris@chrispynegar.co.uk>
2016-07-13 11:20:48 +0100

    Added route groups

--- src/Spire/Routing/Route.php
+++ src/Spire/Routing/Route.php
@@ -1,8 +1,15 @@
 <?php
 namespace Spire\Routing;
 
+use \Closure;
+
 class Route
 {
+
+    /**
+     * @var string Route prefix.
+     */
+    private static $prefix = '';
 
     /**
      * @var string The module we're setting routes for.
@@ -18,17 +25,7 @@ class Route
      */
     public static function get(string $uri, array $options): bool
     {
-        if (static::validateOptions($options))
-        {
-            // Set module.
-            if (!isset($options['module'])) $options['module'] = static::$module;
-
-            // Set route.
-            Repository::store('get', $uri, $options);
-            return true;
-        }
-
-        return false;
+        return static::add('get', $uri, $options);
     }
 
     /**
@@ -38,30 +35,83 @@ class Route
      * @param  string  $options  The route options.
      * @return bool
      */
-     public static function post(string $uri, array $options): bool
-     {
-         if (static::validateOptions($options))
-         {
-             // Set module.
-             if (!isset($options['module'])) $options['module'] = static::$module;
+    public static function post(string $uri, array $options): bool
+    {
+        return static::add('post', $uri, $options);
+    }
 
-             // Set route.
-             Repository::store('post', $uri, $options);
-             return true;
-         }
+    /**
+     * Creates a group of routes with a particular prefix.
+     *
+     * @param  string    $prefix  The route prefix.
+     * @param  function  $routes  The routes callback.
+     * @return void
+     */
+    public static function group(string $prefix, Closure $routes)
+    {
+        // Define the prefix.
+        static::$prefix = $prefix;
 
-         return false;
-     }
+        // Set the routes.
+        $routes();
 
-     /**
-      * Validates the route options.
-      *
-      * @param  array  $options  Route options to validate.
-      * @return bool
-      */
-     private static function validateOptions(array $options): bool
-     {
-         return isset($options['controller'], $options['action']);
-     }
+        // Empty the prefix.
+        $prefix = '';
+    }
+
+    /**
+     * Sets a route.
+     *
+     * @param  string  $method   The route method,
+     * @param  string  $uri      The URI to route.
+     * @param  string  $options  The route options.
+     * @return bool
+     */
+    public static function add(string $method, string $uri, array $options): bool
+    {
+        if (static::validateOptions($options))
+        {
+            // Set module.
+            if (!isset($options['module'])) $options['module'] = static::$module;
+
+            // Set route.
+            Repository::store($method, static::prefixed($uri), $options);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Preprends the prefix to the URI.
+     *
+     * @param  string  $uri  The URI to prefix.
+     * @return string
+     */
+    private static function prefixed(string $uri): string
+    {
+        // Normalize the URI.
+        $uri = trim($uri, '/');
+
+        // Prepend prefix if its set.
+        if (static::$prefix !== '')
+        {
+
+            $uri = trim(static::$prefix, '/') . '/' . $uri;
+        }
+
+        return $uri;
+    }
+
+    /**
+     * Validates the route options.
+     *
+     * @param  array  $options  Route options to validate.
+     * @return bool
+     */
+    private static function validateOptions(array $options): bool
+    {
+        return isset($options['controller'], $options['action']);
+    }
 
 }
